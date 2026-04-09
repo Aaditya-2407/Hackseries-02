8a1e4020ce2c14b2d166709ccd30dde4981a07a5
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-04-09 14:20:47 +0530

    Revert "adjusted size of phonix"
    This reverts commit 4aaaa98dc7e04a422abe2b2429575121c7cb7f15.

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -111,7 +111,7 @@ const Hero = () => {
                             <img
                                 src={PhoenixLogo}
                                 alt="Phoenix"
-                                className="w-full h-full object-contain brightness-90 lg:brightness-150 contrast-125 drop-shadow-[0_0_30px_rgba(212,175,55,0.4)]"
+                                className="w-lg h-128 sm:w-160 sm:h-160 md:w-3xl md:h-192 lg:w-lg lg:h-128 xl:w-xl xl:h-144 object-contain brightness-90 lg:brightness-150 contrast-125 drop-shadow-[0_0_30px_rgba(212,175,55,0.4)] max-w-none lg:max-w-full opacity-40 lg:opacity-100"
                             />
                             <motion.div
                                 animate={{ opacity: [0.1, 0.4, 0.1] }}
